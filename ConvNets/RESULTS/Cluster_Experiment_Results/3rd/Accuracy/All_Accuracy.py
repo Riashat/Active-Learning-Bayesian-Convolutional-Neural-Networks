56c13e8471c1d3583859740e712fa4baf8152f3d
Riashat <riashat.islam.93@gmail.com>
2016-06-01 12:06:44 +0100

    3rd Exp Results Included

--- ConvNets/RESULTS/Cluster_Experiment_Results/3rd/Accuracy/All_Accuracy.py
+++ ConvNets/RESULTS/Cluster_Experiment_Results/3rd/Accuracy/All_Accuracy.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 Bald = np.load('BALD.npy')
-# Highest_Entropy = np.load('Max_Entropy_2nd_All_Accuracy_Results_Experiment_0.npy')
-# Bayes_Segnet = np.load('Segnet2nd_All_Accuracy_Results_Experiment_0.npy')
-# BCNN_Entropy = np.load('Dropout_Max_Entropy_2nd_Run_All_Accuracy_Results_Experiment_0.npy')
-# Random = np.load('Random_2nd_Average_Accuracy.npy')
+Highest_Entropy = np.load('Max_Entropy.npy')
+Bayes_Segnet = np.load('Segnet.npy')
+BCNN_Entropy = np.load('Dropout_Max_Entropy.npy')
+Random = np.load('Random.npy')
 # Variation = np.load('Variation_Ratio_2nd_Run_All_Accuracy_Results_Experiment_0.npy')
 
 
@@ -16,11 +16,12 @@ Queries = np.arange(0, 1600, 100)
 plt.figure(figsize=(12, 8), dpi=80)
 
 plt.plot(Queries, Bald, color="red", linewidth=1.0, marker='x', label="Dropout BALD " )
-# plt.plot(Queries, Bayes_Segnet, color="blue", linewidth=1.0, marker='x', label="Dropout Bayes Segnet")
-# plt.plot(Queries, BCNN_Entropy, color="black", linewidth=1.0, marker='x', label="Dropout - Max Entropy")
-# plt.plot(Queries, Highest_Entropy, color="green",linewidth=1.0, marker='x', label="Max Entropy" )
-# plt.plot(Queries, Random, color="magenta",linewidth=1.0, marker='x', label="Random Acquisition" )
-# plt.plot(Queries, Variation, color="yellow",linewidth=1.0, marker='x', label="Dropout - Variation Ratio" )
+plt.plot(Queries, Bayes_Segnet, color="blue", linewidth=1.0, marker='x', label="Dropout Bayes Segnet")
+plt.plot(Queries, BCNN_Entropy, color="black", linewidth=1.0, marker='x', label="Dropout - Max Entropy")
+plt.plot(Queries, Highest_Entropy, color="green",linewidth=1.0, marker='x', label="Max Entropy" )
+plt.plot(Queries, Random, color="magenta",linewidth=1.0, marker='x', label="Random Acquisition" )
+
+#plt.plot(Queries, Variation, color="yellow",linewidth=1.0, marker='x', label="Dropout - Variation Ratio" )
 
 
 plt.xlabel('Number of Queries')
